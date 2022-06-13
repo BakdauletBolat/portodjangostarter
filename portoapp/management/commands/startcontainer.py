45c0e09fb448935f8d7e2a9882363afb06206806
evrika <evrika@evrikas-MacBook-Air.local>
2022-06-13 12:41:10 +0600

    added to pypi

--- portoapp/management/commands/startcontainer.py
+++ portoapp/management/commands/startcontainer.py
@@ -3,13 +3,15 @@ from django.core.management.templates import TemplateCommand
 
 
 class Command(TemplateCommand):
-    help = "Starts an app the porto way"
+    help = "Starts a container the porto way"
 
     def handle(self, *args, **options):
-        app_name = options.pop("name")
+        app_name = options.pop("name",None)
+
         target = options.pop("directory")
         options["template"] = os.path.join(
             os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "_app_template"
         )
         super().handle("app", app_name, target, **options)
         print(f"Creating {app_name}")
+        self.stdout.write(self.style.SUCCESS(f'- Successfully created porto container - {app_name} 🚢'))
